039b9d09475611106895b00873f7bd2fa7814547
Helen Rottluff <Helen.Rottluff@web.de>
2018-07-07 12:21:30 +0100

    My Django Girls all, first commit

---
+++ introduction/intro1.py
@@ -0,0 +1,14 @@
+print ('Hello, Django girls!')
+"Hi there" + "Django girls!"
+"haha" *3
+"start".upper()
+len ("Supercalifragilistischexplialigetisch")
+coder = "Helen"
+len(coder)
+print(coder)
+luckywheel = [3,66,7,89,121]
+len(luckywheel)
+luckywheel.sort()
+denmark = {'capital': 'copenhagen', 'inhabitants': [6000000],
+'language': 'danish"}
+print (denmark('capital'))
---
+++ introduction/intro2.py
@@ -0,0 +1,59 @@
+# print ('Hello, Django girls!')
+# if 3>2: 
+#     print ('It works!')
+# if 5>2:
+#     print('5 is indeed greater than 2')
+# else:
+#     print('5 is not greater than 2') 
+# name = 'Ronja'
+# if name == 'Elli':
+#     print ('Hey Elli!')
+# elif name == 'Ronja':
+#     print ('Hey Ronja!')
+# else:
+#     print ('Hey anonymous!')  
+volume = 5
+if volume < 20:
+    print ("It's kinda quiet.")
+elif 20 <= volume < 40:
+    print ("It's nice for backround music.")
+elif 40 <= volume < 60:
+    print ("Perfect, I can hear all the details.")
+elif 60 <= volume < 80:
+    print("Nice for parties.")
+elif 80 <= volume < 100:
+    print ("A bit loud!")
+else:
+    print ("My ears are hurting!:(")
+# volume = 30
+# if volume < 20:
+#     print ("It's kinda quiet.")
+# else: 
+#     if 20 <= volume < 40:
+#         print ("It's nice for backround music.")
+#     else: 
+#         print ("My ears are hurting.")
+# Change the volume if it's too loud or too quiet
+# if volume < 20 or volume > 80:
+#     volume = 50
+#     print ("That's better!")
+# def hi():
+#     print ('Hi there!')
+#     print ('How are you?')
+# def hi(name):
+#     if name == 'Helen':
+#         print ('Hi Helen!')
+#     elif name == 'Sophie':
+#         print ('Hi Sophie!')
+#     else:
+#         print ('Hi anonymous!')
+# hi("Sophie")
+# hi("Solveig")
+# def hi(name):
+#     print ('Hi ' + name + '!')
+# girls = ['Rachel', 'Tamara', 'Shinah','Lisa', 'Helen']
+# for name in girls:
+#     hi(name)
+#     print ('Next girl')
+for i in range (1,6):
+    print (i)
